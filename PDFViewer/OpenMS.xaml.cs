35c8589177218d434012f4476c4010c431dd23e8
SapiensAnatis <jay0@me.com>
2017-03-23 16:26:18 +0000

    Add project files.

---
+++ PDFViewer/OpenMS.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+
+
+/* Naming conventions:
+ * CamelCase (ThisIsAnExample) with underscores to differentiate between events and acronyms
+ * e.g. MS_BrowseButton_Click - MS is an acronum, and Click is the event
+ * local variables may be in lowercase
+*/
+
+namespace PDFViewer
+{
+    /// <summary>
+    /// Interaction logic for Open.xaml
+    /// </summary>
+    public partial class OpenMS : Window
+    {
+        public string QP_Path { get; set; }
+
+        private string MS_Path
+        {
+            get
+            {
+                if ((bool)MS_UseFilepath.IsChecked) { return MS_FilepathTextBox.Text; }
+                else if ((bool)MS_Use_URL.IsChecked) { return MS_URL_TextBox.Text; }
+                else { return null; }
+            }
+        }
+
+        public OpenMS(string QP_Path_In)
+        {
+            InitializeComponent();
+            QP_Path = QP_Path_In;
+        }
+
+        private void MS_BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            MS_FilepathTextBox.Text = BrowseForFilepath(".pdf", "Mark schemes|*.pdf|All files|*");
+        }
+
+        public static string BrowseForFilepath(string defaultExt, string filters)
+        {
+            Microsoft.Win32.OpenFileDialog OpenPDFDialogue = new Microsoft.Win32.OpenFileDialog(); // Win32
+
+            OpenPDFDialogue.DefaultExt = defaultExt;
+            OpenPDFDialogue.Filter = filters;
+
+            bool? Result = OpenPDFDialogue.ShowDialog();
+
+            if (Result.HasValue && Result.Value) // Perform validity checks
+            {
+                return OpenPDFDialogue.FileName;
+            }
+            else { return null; }
+        }
+    }
+}
---
+++ PDFViewer/OpenQP.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+
+namespace PDFViewer
+{
+    /// <summary>
+    /// Interaction logic for Open.xaml
+    /// </summary>
+    public partial class OpenQP : Window
+    {
+        private string QP_Path
+        {
+            get
+            {
+                if ((bool)QP_UseFilepath.IsChecked) { return QP_FilepathTextBox.Text; }
+                else if ((bool)QP_Use_URL.IsChecked) { return QP_URL_TextBox.Text; }
+                else { return null; }
+            }
+        }
+
+        public OpenQP()
+        {
+            InitializeComponent();
+        }
+
+        private void QP_BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            QP_FilepathTextBox.Text = BrowseForFilepath(".pdf", "Question papers |*.pdf|All files|*");
+        }
+
+        private void QP_ProceedButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Check that the filepaths and stuff are valid before we proceed. 
+            // Also scan for mark scheme and only do the following if we can't find it
+
+            var MSPrompt = new OpenMS(QP_Path);
+            MSPrompt.Show();
+            Close();
+        }
+
+        public static string BrowseForFilepath(string defaultExt, string filters)
+        {
+            Microsoft.Win32.OpenFileDialog OpenPDFDialogue = new Microsoft.Win32.OpenFileDialog(); // Win32
+
+            OpenPDFDialogue.DefaultExt = defaultExt;
+            OpenPDFDialogue.Filter = filters;
+
+            bool? Result = OpenPDFDialogue.ShowDialog();
+
+            if (Result.HasValue && Result.Value) // Perform validity checks
+            {
+                return OpenPDFDialogue.FileName;
+            }
+            else { return null; }
+        }
+    }
+}
