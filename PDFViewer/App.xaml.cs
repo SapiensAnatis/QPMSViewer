f643a18a7fd4d163fb084d018a1849ecf29eaa8f
SapiensAnatis <jay0@me.com>
2017-03-23 16:51:07 +0000

    Add framework for URL downloading
    Also restructuring, moved common methods to App.xaml, download code will
    likely be here too

--- PDFViewer/App.xaml.cs
+++ PDFViewer/App.xaml.cs
@@ -13,5 +13,23 @@ namespace PDFViewer
     /// </summary>
     public partial class App : Application
     {
+        public static class FileGrabbingUtils
+        {
+            public static string BrowseForFilepath(string defaultExt, string filters)
+            {
+                Microsoft.Win32.OpenFileDialog OpenPDFDialogue = new Microsoft.Win32.OpenFileDialog(); // Win32
+
+                OpenPDFDialogue.DefaultExt = defaultExt;
+                OpenPDFDialogue.Filter = filters;
+
+                bool? Result = OpenPDFDialogue.ShowDialog();
+
+                if (Result.HasValue && Result.Value) // Perform validity checks
+                {
+                    return OpenPDFDialogue.FileName;
+                }
+                else { return null; }
+            }
+        }
     }
 }
--- PDFViewer/OpenMS.xaml.cs
+++ PDFViewer/OpenMS.xaml.cs
@@ -35,23 +35,7 @@ namespace PDFViewer
 
         private void MS_BrowseButton_Click(object sender, RoutedEventArgs e)
         {
-            MS_FilepathTextBox.Text = BrowseForFilepath(".pdf", "Mark schemes|*.pdf|All files|*");
-        }
-
-        public static string BrowseForFilepath(string defaultExt, string filters)
-        {
-            Microsoft.Win32.OpenFileDialog OpenPDFDialogue = new Microsoft.Win32.OpenFileDialog(); // Win32
-
-            OpenPDFDialogue.DefaultExt = defaultExt;
-            OpenPDFDialogue.Filter = filters;
-
-            bool? Result = OpenPDFDialogue.ShowDialog();
-
-            if (Result.HasValue && Result.Value) // Perform validity checks
-            {
-                return OpenPDFDialogue.FileName;
-            }
-            else { return null; }
+            MS_FilepathTextBox.Text = App.FileGrabbingUtils.BrowseForFilepath(".pdf", "Mark schemes|*.pdf|All files|*");
         }
     }
 }
